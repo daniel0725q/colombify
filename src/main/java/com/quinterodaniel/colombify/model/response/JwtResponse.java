ce560eeffabb49f8f87533b79e89ee4f95149302
Daniel Quintero <daniel0725q@gmail.com>
2024-03-17 22:36:54 -0500

    Changed token response

--- src/main/java/com/quinterodaniel/colombify/model/response/JwtResponse.java
+++ src/main/java/com/quinterodaniel/colombify/model/response/JwtResponse.java
@@ -3,14 +3,17 @@ package com.quinterodaniel.colombify.model.response;
 import lombok.Getter;
 
 import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
 
 @Getter
 public class JwtResponse implements Serializable {
 
     private static final long serialVersionUID = -8091879091924046844L;
-    private final String token;
+    private final Map data;
 
     public JwtResponse(String jwttoken) {
-        this.token = jwttoken;
+        this.data = new HashMap();
+        this.data.put("token", jwttoken);
     }
 }
